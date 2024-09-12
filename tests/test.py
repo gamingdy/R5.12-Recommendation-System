e16a4703a773f31796c5fda36af3074b4b962330
Gamingdy <christgik12@gmail.com>
2024-09-12 12:25:05 +0200

    feat: add TestData

--- tests/test.py
+++ tests/test.py
@@ -1,6 +1,7 @@
+import os
 import unittest
 
-from src.utils import pearson_similarity
+from src.utils import data_reader, get_user_data, pearson_similarity
 
 
 class TestPearson(unittest.TestCase):
@@ -22,6 +23,20 @@ class TestPearson(unittest.TestCase):
         x = [1, 2, 3, 4, 5]
         y = [1, 1, 1, 1, 1]
         self.assertEqual(pearson_similarity(x, y), 0)
+
+
+class TestData(unittest.TestCase):
+    def setUp(self):
+        self.dirname = os.path.split(os.path.abspath(__file__))[0]
+
+    def test_data_reader(self):
+        data = data_reader(f"{self.dirname}/dataset/test.xlsx")
+        self.assertEqual(data.shape, (8, 1))
+
+    def test_get_user_data(self):
+        data = data_reader(f"{self.dirname}/dataset/test.xlsx")
+        expected = [0, 4, 2, 1, 4, 0, 2, 1, 4, 3, 3, 2, 4, 1, 3, 1, 2, 0, 4, 1]
+        self.assertEqual(get_user_data(data, 2), expected)
 
 
 if __name__ == "__main__":
