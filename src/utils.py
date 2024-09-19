da8cd394f7c450bba8617f330bfe03da5ad722c6
Gamingdy <christgik12@gmail.com>
2024-09-19 12:05:16 +0200

    fix: fix de pearson_similarity

--- src/utils.py
+++ src/utils.py
@@ -7,15 +7,18 @@ def pearson_similarity(u1, u2):
         raise ValueError("Les deux listes doivent avoir la même longueur")
     u1 = np.array(u1)
     u2 = np.array(u2)
+
     mean_u1 = np.mean(u1)
     mean_u2 = np.mean(u2)
+
     numerator = np.sum((u1 - mean_u1) * (u2 - mean_u2))
     denominator = np.sqrt(
-        np.sum((u1 - mean_u1) ** 2)) *  np.sqrt(np.sum((u2 - mean_u2) ** 2)
+        np.sum((u1 - mean_u1) ** 2) * np.sum((u2 - mean_u2) ** 2)
     )
+
     if denominator == 0:
         return 0
-    result = numerator/denominator
+    result = numerator / denominator
     return result
 
 
@@ -23,13 +26,13 @@ def prediction(utilisateur_a_noter, item, liste_u_notes):
     numerator = moyenne(utilisateur_a_noter)
     denominator = 0
     for utilisateur in liste_u_notes:
-        u1,u2 = comparer(utilisateur_a_noter, utilisateur)
-        pearson=pearson_similarity(u1,u2)
+        u1, u2 = comparer(utilisateur_a_noter, utilisateur)
+        pearson = pearson_similarity(u1, u2)
         numerator += (note(utilisateur, item) - moyenne(utilisateur)) * pearson
         denominator += pearson
     if denominator == 0:
         return -1
-    return numerator / denominator
+    return abs(numerator / denominator)
 
 
 def data_reader(file_path) -> pd.DataFrame:
@@ -39,12 +42,11 @@ def data_reader(file_path) -> pd.DataFrame:
 def get_liste_utilisateur(data, item):
     resultat = []
     for i, _ in data.iterrows():
-        user =get_user_data(data,i)
+        user = get_user_data(data, i)
         if user[item] != -1:
             resultat.append(user)
-    
+
     return resultat
-    
 
 
 def moyenne(utilisateur):
@@ -62,14 +64,16 @@ def get_user_data(data, user_id):
 def filtrer(utilisateur):
     return list(filter(lambda x: (x > -1), utilisateur))
 
-def comparer(u1,u2):
-    result_u1=[]
-    result_u2=[]
-    for indice,item in enumerate(u1):
-        if u2[indice]!=-1 and item!=-1:
+
+def comparer(u1, u2):
+    result_u1 = []
+    result_u2 = []
+    for indice, item in enumerate(u1):
+        if u2[indice] != -1 and item != -1:
             result_u1.append(item)
             result_u2.append(u2[indice])
-    return(result_u1,result_u2)
+    return result_u1, result_u2
+
 
 def cosine_similarity(u1, u2):
     u1 = np.array(u1)
@@ -80,5 +84,3 @@ def cosine_similarity(u1, u2):
     if norm_u1 == 0 or norm_u2 == 0:
         return 0
     return dot_product / (norm_u1 * norm_u2)
-
-
